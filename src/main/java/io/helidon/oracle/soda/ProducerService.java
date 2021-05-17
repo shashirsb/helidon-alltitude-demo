6cbc20067da1d2004bbdaa573213b49d7026e132
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2021-05-17 22:10:11 +0530

    adding pom and wallet

--- src/main/java/io/helidon/oracle/soda/ProducerService.java
+++ src/main/java/io/helidon/oracle/soda/ProducerService.java
@@ -19,7 +19,7 @@ public class ProducerService {
 	    //private static boolean UseDB = true;
 	    private final static String ATP_CONNECT_NAME = "altdb_medium";
 	    //private final static String ATP_PASSWORD_FILENAME = "atp_password.txt";
-	    private final static String WALLET_LOCATION = "C:/oracle/engagement/2020-21/json4alltitude/code/Wallet_ALTDB";
+	    private final static String WALLET_LOCATION = "/home/opc/Wallet_ALTDB";
 	    private final static String DB_URL = "jdbc:oracle:thin:@" + ATP_CONNECT_NAME + "?TNS_ADMIN=" + WALLET_LOCATION;
 	    private final static String DB_USER = "admin";
 	    private static String DB_PASSWORD = "zT7_P53Ia1_A";
